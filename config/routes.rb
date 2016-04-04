26e9994974a4f3c4760df835fabc60d3953f5f2b
fatbulat <fatbulat5@gmail.com>
2016-04-04 12:46:55 +0300

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+
+  root 'welcome#index'
+
+  namespace :api do
+    devise_for :users, { 
+      singular: :user,
+      controllers: {
+        passwords: 'devise/passwords',
+        registrations: 'devise/registrations',
+        sessions: 'devise/sessions'
+      }
+    }
+  end
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
